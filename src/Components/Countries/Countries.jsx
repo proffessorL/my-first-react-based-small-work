0ec1a344f1c5a248bd42f78b28b293b4ad147359
proffessorL <professorl6868@gmail.com>
2025-09-19 20:19:35 +0600

    aded flags population from api

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -11,7 +11,8 @@ const Countries = ({countriesPromise}) => {
         <div>
             <h1>In The Countries : {countries.length}</h1>
             {
-                countries.map(country => <Country country={country}></Country>)
+                countries.map(country =>
+                     <Country key={country.cca3.cca3} country={country}></Country>)
             }
         </div>
     );
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 const Country = ({country}) => {
 
     // console.log(country.name.common)
+    // console.log(country.flags.flags.png)
 
     return (
         <div>
             <h3> Name : {country.name.common} </h3>
+            <h3> Population : {country.population.population}</h3>
+            <img src={country.flags.flags.png} alt="" />
         </div>
     );
 };
