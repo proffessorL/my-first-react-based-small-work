c1e05e8686db7358ea2a6d94f0098c9c80840d98
proffessorL <professorl6868@gmail.com>
2025-09-19 20:43:34 +0600

    Added some css on react application

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,19 +1,22 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import "./Country.css"
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
     const countriesData = use(countriesPromise)
     const countries = countriesData.countries
 
-    
 
-    return ( 
+
+    return (
         <div>
             <h1>In The Countries : {countries.length}</h1>
-            {
-                countries.map(country =>
-                     <Country key={country.cca3.cca3} country={country}></Country>)
-            }
+            <div className='countries'>
+                {
+                    countries.map(country =>
+                        <Country key={country.cca3.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-
+import "./Country.css"
 const Country = ({country}) => {
 
     // console.log(country.name.common)
     // console.log(country.flags.flags.png)
 
+    const handleVisit = () => {
+        console.log("Button clicked")
+    }
+
     return (
-        <div>
+        <div className='country'>
+             <img src={country.flags.flags.png} alt="" />
             <h3> Name : {country.name.common} </h3>
             <h3> Population : {country.population.population}</h3>
-            <img src={country.flags.flags.png} alt="" />
+            <p> Area : {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+           <button onClick={handleVisit}>Not Visited</button>
         </div>
     );
 };
